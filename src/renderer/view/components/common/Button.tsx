fb709bb23daa0a522f5b0b5532c184f005ac5376
artinc5233 <artinc5233@samlab.co.kr>
2022-09-11 17:07:55 +0900

    로그인 마크업 구현

--- src/renderer/view/components/common/Button.tsx
+++ src/renderer/view/components/common/Button.tsx
@@ -2,23 +2,29 @@ import React from 'react';
 import theme from 'renderer/assets/global/theme';
 import styled from 'styled-components';
 
-const ButtonContainer = styled.button`
-  background-color: ${theme.main.purple};
+const ButtonContainer = styled.button<{isActive:boolean}>`
+  background-color:${props => props.isActive? props.theme.main.purple:props.theme.main.gray};
+  border: none;
+  width: 100%;
   height: 56px;
-  width: 104px;
   left: 467px;
   top: 87px;
   border-radius: 4px;
   padding: 10px 20px 10px 20px;
+  >span{
+    color: #fff;
+  }
 `;
 
 interface IProps {
   item: string;
   onClick: () => void;
+  isActive:boolean;
+  style?:React.CSSProperties
 }
 
-const Button = ({ item, onClick }: IProps) => {
-  return <ButtonContainer onClick={() => onClick()}>{item}</ButtonContainer>;
+const Button = ({ item, onClick, isActive,style }: IProps) => {
+  return <ButtonContainer style={style} isActive={isActive} onClick={() => onClick()}><span>{item}</span> </ButtonContainer>;
 };
 
 export default Button;
