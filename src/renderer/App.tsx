3bc53a8eaf89415f3fae229739f70f89fd159fb1
artinc5233 <artinc5233@samlab.co.kr>
2022-09-11 14:04:39 +0900

    1. router 추가 2. styled-componets 추가 3. eslint 설정

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,15 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './assets/global/GlobalStyle';
+import { theme } from './assets/global/theme';
+import Router from './routes';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+    <GlobalStyle/>
+   <Router/>
+   </ThemeProvider>
   );
 }
---
+++ src/renderer/routes/routes.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from 'renderer/view/pages/home';
+import Login from 'renderer/view/pages/Login';
+
+interface RouterDefine {
+  path: string;
+  Component: () => JSX.Element;
+}
+
+const ROUTER: RouterDefine[] = [
+  {
+    path: '/login',
+    Component: Login,
+  },
+  {
+    path: '/',
+    Component: Home,
+  },
+];
+
+export default ROUTER;
