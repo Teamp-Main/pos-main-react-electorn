50eebe5001f3417dd079d0e807e9a450b44800a1
luminousxv <drgvyhn@gmail.com>
2022-09-11 17:09:38 +0900

    공용컴포넌트 Input 적용 gap 및 ui 요소 수정

--- src/renderer/view/components/signup/input-button.tsx
+++ src/renderer/view/components/signup/input-button.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../common/Button";
+import Input from "../common/Input";
 
 const InputButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 10px;
+  gap: 100px;
 `
 
 interface IProps {
   item: string;
+  label: string;
+  state: string;
+  inputType:'text' | 'number' | 'password'
+  setState: React.Dispatch<React.SetStateAction<string >>
 }
 
-const InputButton = ({item}: IProps) => {
+const InputButton = ({item, label, state, inputType, setState}: IProps) => {
   const onClick = () => {};
   return (
     <InputButtonWrapper>
-      <input />
+      <Input label={label} state={state} inputType={inputType} setState={setState}/>
       <Button item={item} onClick={onClick} />
     </InputButtonWrapper>
   )
--- src/renderer/view/pages/SignUp.tsx
+++ src/renderer/view/pages/SignUp.tsx
@@ -1,33 +1,44 @@
 //import React from 'react';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../components/common/Button';
+import Input from '../components/common/Input';
 import InputButton from '../components/signup/input-button';
 
 const SignUpWrapper = styled.div`
   display: flex;
-  height: 100vh;
+  width: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 100vh;
 `;
 
 const SignUpContainer = styled.div`
   display: flex;
+  width: 50%;
   flex-direction: column;
-  gap: 10px;
+  gap: 30px;
 `
 
 const SignUp = () => {
+  const [nameinput, setNameInput] = useState<string>('');
+  const [idinput, setIdInput] = useState<string>('');
+  const [pwinput, setPwInput] = useState<string>('');
+  const [pwcheckinput, setPwCheckInput] = useState<string>('');
+  const [emailinput, setEmailInput] = useState<string>('');
+  const [authnuminput, setAuthNumInput] = useState<string>('');
+
   const onClick = () => {};
   return (
     <SignUpWrapper>
       <SignUpContainer>
-        <input />
-        <InputButton item='중복확인' />
-        <input />
-        <input />
-        <InputButton item='인증받기' />
-        <InputButton item='인증확인' />
+        <Input label='이름' state={nameinput} setState={setNameInput} inputType='text'/>
+        <InputButton label='아이디' item='중복확인' state={idinput} inputType='text' setState={setIdInput} />
+        <Input label='비밀번호' state={pwinput} setState={setPwInput} inputType='password' />
+        <Input label='비밀번호 확인' state={pwcheckinput} setState={setPwCheckInput} inputType='password' />
+        <InputButton item='인증받기' label='이메일' state={emailinput} setState={setEmailInput} inputType='text' />
+        <InputButton item='인증확인' label='인증번호' state={authnuminput} setState={setAuthNumInput} inputType='text' />
         <Button item="가입하기" onClick={onClick} />
         </SignUpContainer>
     </SignUpWrapper>
